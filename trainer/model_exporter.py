8d7a24256f276a9f0890f6fabe09c0581f9e0171
Devpriya Kulemannege <80089198+devkulemannege@users.noreply.github.com>
2026-06-30 15:12:35 +0530

    Add model exporter, dataset and trained model
    Adds a simple training pipeline and artifact: trainer/model_exporter.py trains a LogisticRegression and exports it to model.pkl.

---
+++ trainer/model_exporter.py
@@ -0,0 +1,13 @@
+import pickle
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+
+df = pd.read_csv('model/training_dataset.csv')
+
+xFeature = df[['lst','sur_refl','ndvi','month']] # extract all rows with features as x
+yPredicted = df['fire'] # extract all rows with "fire" col for x
+
+model = LogisticRegression()
+model.fit(xFeature, yPredicted)
+
+pickle.dump(model, open('model.pkl','wb')) # export

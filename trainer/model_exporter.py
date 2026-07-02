9029fd1b4bd08c539fd670359f776f2911d49003
Devpriya Kulemannege <80089198+devkulemannege@users.noreply.github.com>
2026-07-02 12:52:31 +0530

    replace dataset with new

--- trainer/model_exporter.py
+++ trainer/model_exporter.py
@@ -2,7 +2,7 @@ import pickle
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 
-df = pd.read_csv('model/training_dataset.csv')
+df = pd.read_csv('trainer/training_dataset.csv')
 
 xFeature = df[['lst','sur_refl','ndvi','month']] # extract all rows under feature cols as x
 yPredicted = df['fire'] # extract all rows with "fire" col for x
